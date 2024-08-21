2f2da1f3375955f7f8763a50be4ef592ec5b5927
Mitesh <mgupta@kiwiup.com>
2024-08-21 12:37:29 +0530

    correcting permission for new contract

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -92,5 +92,9 @@ module.exports = async function (deployer) {
   const receipt4 = await permissionsManager.addRole(3, scSeasonRewards.address, { from: superchampFoundationAddress }) //    await permissionsManagerInstance.addRole(role, accountToAdd, { from: accounts[0] }); // Use appropriate account as the sender
   console.log("permissionsManager transfer admin success to contract scSeasonRewards");
 
+  const receipt5 = await permissionsManager.addRole(3, scMetaGamePool.address, { from: superchampFoundationAddress }) //    await permissionsManagerInstance.addRole(role, accountToAdd, { from: accounts[0] }); // Use appropriate account as the sender
+  console.log("permissionsManager transfer admin success to contract scMetaGamePool");
+
+
   //todo : implement from `FACTORY.initializeEmmissions is called by Foundation Multisig for Metagame system` part of  https://docs.google.com/document/d/1uMl_cJhMeJL0ND6i7eMOf73JtRQ5qnMTpeyNra343yI/edit#heading=h.gll6bjs55xo8
 };
