b68b1126de8554792f502434cf452f8da1996227
Sriharsha <sriharsha@useblitz.com>
2025-07-12 02:35:19 +0530

    base network added

--- hardhat.config.js
+++ hardhat.config.js
@@ -39,6 +39,11 @@ module.exports = {
       chainId: 84532,
       accounts: [`0x${process.env.PRIVATEKEY}`],
     },
+    base: {
+      url: `${process.env.RPC_URL}`, // Base Sepolia Testnet RPC
+      chainId: 8453,
+      accounts: [`0x${process.env.PRIVATEKEY}`],
+    },
     fuji: {
       url: `${process.env.RPC_URL}`, // Fuji Testnet RPC
       chainId: 43113,
@@ -55,6 +60,14 @@ module.exports = {
         urls: {
           apiURL: "https://api.etherscan.io/v2/api?chainid=84532",
           browserURL: "https://sepolia.basescan.org",
+        },
+      },
+      {
+        network: "base",
+        chainId: 8453,
+        urls: {
+          apiURL: "https://api.etherscan.io/v2/api?chainid=8453",
+          browserURL: "https://basescan.org",
         },
       },
     ],
