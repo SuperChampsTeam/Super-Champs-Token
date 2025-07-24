d72769cd1ecff434871e3f1b5c5ed2be61fb3b31
ssri4862 <101623143+ssri4862@users.noreply.github.com>
2025-07-24 19:37:54 +0530

    Add files via upload
    interfaces added

---
+++ SuperChamps/interfaces/IKigu.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.20;
+
+interface IKigu {
+
+
+   function balanceOf(address owner) external view returns (uint);
+
+    function mint(address to, uint amount) external returns (bool);
+
+    function transfer(address to, uint value) external returns (bool);
+
+    function transferFrom(address from, address to, uint value) external returns (bool);
+
+    function approve(address spender, uint value) external returns (bool);
+}
---
+++ SuperChamps/interfaces/IMinter.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.20;
+
+/// @title IMinter - Interface for the MinterUpgradeable contract
+interface IMinter {
+    function updatePeriod() external returns (uint);
+
+    function mintInitialSupply() external;
+
+    function setDistributionWallets(address[4] calldata wallets) external;
+
+    function setDistributionPercents(uint256[4] calldata percents) external;
+}
