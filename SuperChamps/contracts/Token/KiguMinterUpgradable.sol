f74dfaa1e7805e55650d6241975f5652daaf3b3a
ssri4862 <101623143+ssri4862@users.noreply.github.com>
2025-07-24 19:36:45 +0530

    Add files via upload
    kgu token and kigu minter added

---
+++ SuperChamps/contracts/Token/KiguMinterUpgradable.sol
@@ -0,0 +1,113 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.20;
+
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "../.././interfaces/IKigu.sol";
+
+contract KiguMinterUpgradeable is OwnableUpgradeable {
+    uint256 public constant INITIAL_SUPPLY = 10_000_000 * 1e18;
+    uint256 public constant FIXED_WEEKLY_SUPPLY = 3_000_000 * 1e18;
+    uint256 public constant MAX_TOTAL_SUPPLY = 200_000_000 * 1e18;
+    uint256 public constant FIXED_MINT_END_WEEK = 30;
+
+    // Distribution in basis points: [66.67%, 13.33%, 13.33%, 6.67%]
+    uint256[4] public distributionPercents;
+
+    address[4] public distributionWallets;
+
+    uint256 public totalMinted;
+    uint256 public epochCount;
+    uint256 public activePeriod;
+    uint256 public WEEK;
+    uint256 public decayPerWeek;
+    uint256 public lastEmission;
+
+    IKigu public _kigu;
+
+    bool public isInitialMintDone;
+
+    event Minted(uint256 indexed week, uint256 amount, uint256 totalMinted);
+
+    constructor() {}
+
+    function initialize(address kiguToken) public initializer {
+        __Ownable_init(_msgSender());
+        _kigu = IKigu(kiguToken);
+        WEEK = 5 minutes;
+        activePeriod = ((block.timestamp + WEEK) / WEEK) * WEEK;
+
+        // Default percentages in basis points
+        distributionPercents = [6667, 1333, 1333, 667];
+        lastEmission = FIXED_WEEKLY_SUPPLY;
+    }
+
+    function setMintingConfig(
+        address[4] memory wallets,
+        uint256[4] memory percents,
+        uint256 decay
+    ) external onlyOwner {
+        require(
+            percents[0] + percents[1] + percents[2] + percents[3] == 10_000,
+            "Invalid distribution percentages"
+        );
+        require(decay > 0, "Decay should be > 0");
+
+        distributionWallets = wallets;
+        distributionPercents = percents;
+        decayPerWeek = decay;
+    }
+
+    //assumption this should not be called on epoch filp
+    function mintInitialSupply() external onlyOwner {
+        require(!isInitialMintDone, "Already minted");
+        _kigu.mint(address(this), INITIAL_SUPPLY);
+        totalMinted += INITIAL_SUPPLY;
+        isInitialMintDone = true;
+    }
+
+    function updatePeriod() external onlyOwner { 
+        require(block.timestamp >= activePeriod + WEEK, "Too early");
+        require(isInitialMintDone, "Initial mint not done");
+        require(totalMinted < MAX_TOTAL_SUPPLY, "Minting complete");
+
+        epochCount += 1;
+        activePeriod = (block.timestamp / WEEK) * WEEK;
+
+        uint256 emission;
+        if (epochCount <= FIXED_MINT_END_WEEK) {
+            emission = FIXED_WEEKLY_SUPPLY;
+        } else {
+            //confirm that 3..2.7 2.4 or 10% of previous
+            //it will be decay of previous minitng not 3million
+            uint256 decayFactor = 100 - decayPerWeek ;
+            emission = (lastEmission * decayFactor) / 100;
+        }
+
+        // Cap emission to not exceed max total
+        if (totalMinted + emission > MAX_TOTAL_SUPPLY) {
+            emission = MAX_TOTAL_SUPPLY - totalMinted;
+        }
+        
+
+        uint minterBalance = _kigu.balanceOf(address(this));
+        emission -= minterBalance;
+        totalMinted += emission;
+        _kigu.mint(address(this), emission);
+
+        lastEmission = emission;
+
+        for (uint256 i = 0; i < 4; i++) {
+            uint256 share = (emission * distributionPercents[i]) / 10_000;
+            require(
+                _kigu.transfer(distributionWallets[i], share),
+                "Transfer failed"
+            );
+        }
+
+        emit Minted(epochCount, emission, totalMinted);
+    }
+
+    function version() external pure returns (string memory) {
+        return "MinterUpgradeable v2.0.0";
+    }
+}
---
+++ SuperChamps/contracts/Token/KiguToken.sol
@@ -0,0 +1,92 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.20;
+
+import "../.././interfaces/IKigu.sol";
+
+contract KiguToken is IKigu {
+    string public constant name = "KIGU";
+    string public constant symbol = "KIGU";
+    uint8 public constant decimals = 18;
+    uint public totalSupply;
+
+    address public minter;
+
+    mapping(address => uint) public balanceOf;
+    mapping(address => mapping(address => uint)) public allowance;
+
+    event Transfer(address indexed from, address indexed to, uint value);
+    event Approval(address indexed owner, address indexed spender, uint value);
+
+    constructor() {
+        minter = msg.sender;
+        _mint(msg.sender, 0);
+    }
+
+
+    function _mint(address _to, uint _amount) internal returns (bool) {
+        totalSupply += _amount;
+        unchecked {
+            balanceOf[_to] += _amount;
+        }
+        emit Transfer(address(0), _to, _amount);
+        return true;
+    }
+
+    function mint(address _to, uint _amount) external override returns (bool) {
+        require(msg.sender == minter, "Only minter can mint");
+        return _mint(_to, _amount);
+    }
+
+    function approve(address _spender, uint _value) external override returns (bool) {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transfer(address _to, uint _value) external override returns (bool) {
+        return _transfer(msg.sender, _to, _value);
+    }
+
+    function transferFrom(address _from, address _to, uint _value) external override returns (bool) {
+        uint allowed = allowance[_from][msg.sender];
+        if (allowed != type(uint).max) {
+            allowance[_from][msg.sender] -= _value;
+        }
+        return _transfer(_from, _to, _value);
+    }
+
+    function _transfer(address _from, address _to, uint _value) internal returns (bool) {
+        balanceOf[_from] -= _value;
+        unchecked {
+            balanceOf[_to] += _value;
+        }
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function setMinter(address _minter) external {
+        require(msg.sender == minter);
+        minter = _minter;
+    }
+
+    function burn(uint256 value) external returns (bool) {
+        _burn(msg.sender, value);
+        return true;
+    }
+
+    function burnFrom(address _from, uint _value) external returns (bool) {
+        uint allowed_from = allowance[_from][msg.sender];
+        if (allowed_from != type(uint).max) {
+            allowance[_from][msg.sender] -= _value;
+        }
+        _burn(_from, _value);
+        return true;
+    }
+
+    function _burn(address _from, uint _amount) internal returns (bool) {
+        totalSupply -= _amount;
+        balanceOf[_from] -= _amount;
+        emit Transfer(_from, address(0x0), _amount);
+        return true;
+    }
+}
