acae07c7defbf24c4d3990dffa08da5427e9f9cf
ssri4862 <101623143+ssri4862@users.noreply.github.com>
2025-07-27 18:51:53 +0530

    Update IKiguMinter.sol

--- SuperChamps/interfaces/IKiguMinter.sol
+++ SuperChamps/interfaces/IKiguMinter.sol
@@ -1,13 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.24;
 
-/// @title IMinter - Interface for the MinterUpgradeable contract
-interface IMinter {
-    function updatePeriod() external returns (uint);
+/// @title IKiguMinter - Interface for the MinterUpgradeable contract
+interface IKiguMinter {
+    function mintKiguToken() external returns (uint);
 
-    function mintInitialSupply() external;
-
-    function setDistributionWallets(address[4] calldata wallets) external;
-
-    function setDistributionPercents(uint256[4] calldata percents) external;
 }
