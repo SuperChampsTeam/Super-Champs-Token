2055b9a2db74d7c2734c6c6c88fc9be80c7bdd1b
ssri4862 <101623143+ssri4862@users.noreply.github.com>
2025-07-27 19:51:08 +0530

    Update IKigu.sol

--- SuperChamps/interfaces/IKigu.sol
+++ SuperChamps/interfaces/IKigu.sol
@@ -1,10 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.24;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Burnable.sol";
 
-interface IKigu is IERC20, IERC20Burnable {
+interface IKigu {
 
     function mint(address to, uint amount) external returns (bool);
 
