50e7d52c7ddf05edc4de38543b33ac0867528967
ssri4862 <101623143+ssri4862@users.noreply.github.com>
2025-07-27 18:51:05 +0530

    Update KiguToken.sol

--- SuperChamps/contracts/Token/KiguToken.sol
+++ SuperChamps/contracts/Token/KiguToken.sol
@@ -1,9 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.24;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Burnable.sol";
-
 contract KiguToken is IKigu {
     string public constant name = "KIGU";
     string public constant symbol = "KIGU";
