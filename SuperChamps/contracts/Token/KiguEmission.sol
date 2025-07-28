b12cd9b70a529ad0e82e1b44c74f99696446a522
Devanand <devanand@Admins-MacBook-Pro-10.local>
2025-07-28 12:52:47 +0530

    mionor changes

--- SuperChamps/contracts/Token/KiguEmission.sol
+++ SuperChamps/contracts/Token/KiguEmission.sol
@@ -15,6 +15,8 @@ contract KiguEmission is OwnableUpgradeable {
 
     address public emissionManager;
 
+    uint8 public constant wallet_length = 4;
+
     event Distributed(uint256 indexed epoch, uint256 emission);
 
     function initialize(address _token) public initializer {
@@ -39,10 +41,16 @@ contract KiguEmission is OwnableUpgradeable {
         address[4] calldata _wallets,
         uint256[4] calldata _percents
     ) external onlyOwner {
+        require(_wallets.length == wallet_length && _percents.length == wallet_length, "lengthSize4Only");
         require(
             _percents[0] + _percents[1] + _percents[2] + _percents[3] == 10_000,
             "Invalid percentages"
         );
+        
+        for (uint8 i = 0; i < wallet_length; i++) {
+            require(_wallets[i] != address(0), "Wallet cannot be zero address");
+        }
+        
         wallets = _wallets;
         percents = _percents;
     }
@@ -50,7 +58,7 @@ contract KiguEmission is OwnableUpgradeable {
     function mintTokenAndDistribute() external onlyOwnerOrEmissionManager {
         uint256 emission = minter.mintKiguToken();
 
-        for (uint256 i = 0; i < 4; i++) {
+        for (uint8 i = 0; i < wallet_length; i++) {
             uint256 share = (emission * percents[i]) / 10_000;
             require(token.transfer(wallets[i], share), "Transfer failed");
         }
