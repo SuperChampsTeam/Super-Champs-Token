5dc79034f4252fed86edf8a85ffbc57c1053469f
ssri4862 <101623143+ssri4862@users.noreply.github.com>
2025-07-28 11:12:34 +0530

    Update IKigu.sol

--- SuperChamps/interfaces/IKigu.sol
+++ SuperChamps/interfaces/IKigu.sol
@@ -5,5 +5,7 @@ pragma solidity 0.8.24;
 interface IKigu {
 
     function mint(address to, uint amount) external returns (bool);
+    function burn(uint256 value) external;
+    function burnFrom(address from, uint256 value) external;
 
 }
